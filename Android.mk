a7cfce97e7463fa37c815f529fdf469b5f2f31e4
root <root@ubuntu.(none)>
2015-07-23 09:01:21 +0800

    init git repository

---
+++ Android.mk
@@ -0,0 +1,28 @@
+# Copyright 2006 The Android Open Source Project
+
+# Setting LOCAL_PATH will mess up all-subdir-makefiles, so do it beforehand.
+SUBDIR_MAKEFILES := $(call all-named-subdir-makefiles,modules tests)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := libcutils
+
+LOCAL_INCLUDES += $(LOCAL_PATH)
+
+LOCAL_CFLAGS  += -DQEMU_HARDWARE
+QEMU_HARDWARE := true
+
+LOCAL_SHARED_LIBRARIES += libdl
+
+LOCAL_SRC_FILES += hardware.c
+
+LOCAL_MODULE:= libhardware
+
+ifeq ($(filter-out exynos5,$(TARGET_BOARD_PLATFORM)),)
+LOCAL_CFLAGS += -DVITHAR_HACK
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(SUBDIR_MAKEFILES)
